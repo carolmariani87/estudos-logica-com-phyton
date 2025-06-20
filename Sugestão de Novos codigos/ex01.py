8abc9267789f258e1ce13b0145f05c1050a447e7
criscarol38 <carolinemarianivendas@gmail.com>
2025-06-20 14:06:46 +0000

    Alterações 2

---
+++ Sugestão de Novos codigos/ex01.py
@@ -0,0 +1,14 @@
+#Crie uma lista chamada 'notas' com 3 valores (as notas dos alunos)
+
+
+notas = [7.5, 8.0, 6.5]
+
+# Some os 3 valores da lista (posição 0, 1 e 2)
+
+soma = notas[0] + notas[1] + notas[2]
+
+ #Divida a soma por 3 para calcular a média
+media = soma / 3
+
+# Mostre o resultado da média na tela
+print("Média das notas:", media)
